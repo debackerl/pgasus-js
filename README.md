66487a506517f0c4b27698f36243d4cc296325e9
Laurent Debacker <debackerl gmail com>
2015-12-22 19:55:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+To access pgasus-backed web service, create a new `WebService` object.
+
+`var ws = new WebService(baseUrl, [username], [password]);`
+
+`username` and `password` are optional parameters if HTTP basic authentication is needed.
+
+`WebService` objects have two methods:
+* `relation(path)` when the resource is a relation (table or view).
+* `procedure(path)` when the resource is a procedure (function).
+
+Objects returned by `relation` have four methods:
+* `get(parameters, filter, order, limit, callback)`
+* `post(parameters, callback)`
+* `put(parameters, filter, callback)`
+* `del(parameters, filter, callback)`, for `delete` HTTP method (delete is a reserved keyword in ECMAScript)
+
+Objects returned by `procedure` have four methods:
+* `get(parameters, callback)`
+* `post(parameters, callback)`
+* `put(parameters, callback)`
+* `del(parameters, callback)`, for `delete` HTTP method (delete is a reserved keyword in ECMAScript)
+
+The provided `callback` must have type `function(error, result)` or be left unspecified. Arguments are:
+* `error`, null if no error detected, otherwise an object with the following members:
+** `httpStatus`, the numeric HTTP status code.
+** `httpStatusText`, the HTTP status message.
+** `description`, additional description of the error may be included here.
+* `result`, if no error has been detected, this will be the deserialized JSON value returned by the remote server.
+
+=== Example ===
+
+var ws = new WebService("https://test.com");
+var login = ws.procedure("/users/:user_id/login").post;
+login({"user_id": "bob", "password": "$3cur3"}, function(res) {
+  var projectTasks = ws.relation("/projects/:project_id/tasks");
+  projectTasks.get({"project_id": 100}, null, QM.Order("dueDate"), null, function(res) { });
+});
---
+++ pgasus-client.js
@@ -0,0 +1,105 @@
+// Copyright (c) 2015, Laurent Debacker
+// MIT License
+
+function WebService(baseUrl, username, password) {
+	this.baseUrl = baseUrl;
+	this.username = username;
+	this.password = password;
+	this.filter = "f";
+	this.sort = "s";
+	this.limit = "l";
+	
+	var ws = this;
+	var xhr = XMLHttpRequest || require("xmlhttprequest").XMLHttpRequest;
+	
+	function load_resource(method, path, parameters, querystring, callback) {
+		var req = new xhr();
+		if(callback)
+			req.onreadystatechange = function() {
+				if(req.readyState == 4) {
+					if(req.status == 200) {
+						callback(null, JSON.parse(req.responseText));
+					} else {
+						callback({"httpStatus": req.status, "httpStatusText": req.statusText, "description": req.responseText});
+					}
+				}
+			};
+		
+		var done = {}, url = ws.baseUrl + path.replace(/\/[:*]([\w_]+)/g, function(_, id) {
+			done[id] = true;
+			return "/" + encodeURIComponent(parameters[id]);
+		}) + ".json";
+		
+		var qs = querystring(done);
+		if(qs.length) url += "?" + qs.join("&");
+		
+		req.open(method, url, true, ws.user, ws.password);
+		
+		switch(method) {
+		case "POST","PUT":
+			var body = {};
+			for(var id in parameters)
+				if(Object.prototype.hasOwnProperty.call(parameters, id) && !done[id])
+					body[id] = parameters[id];
+			
+			req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+			req.send(JSON.stringify(body));
+			break;
+		default:
+			req.send();
+			break;
+		}
+	}
+	
+	function omap(f) {
+		var res = {}, o = {"get":"GET","put":"PUT","post":"POST","del":"DELETE"};
+		for(var id in o) res[id] = f(o[id]);
+		return res;
+	}
+	
+	return {
+		"relation": function(path) {
+			var res = omap(function(method) {
+				return function(parameters, filter, callback) {
+					load_resource(method, path, parameters, function() {
+						var qs = [];
+						if(filter) qs.push(ws.filter + "=" + filter);
+						return qs;
+					}, callback);
+				};
+			});
+			res.get = function(parameters, filter, sort, limit, callback) {
+				load_resource("GET", path, parameters, function() {
+					var qs = [];
+					if(filter) qs.push(ws.filter + "=" + filter);
+					if(sort) qs.push(ws.sort + "=" + sort);
+					if(typeof(limit) === "number") qs.push(ws.limit + "=" + limit);
+					return qs;
+				}, callback);
+			};
+			res.post = function(parameters, callback) {
+				load_resource("POST", path, parameters, function() {
+					return [];
+				}, callback);
+			};
+			return res;
+		},
+		"procedure": function(path) {
+			return omap(function(method) {
+				return function(parameters, callback) {
+					load_resource(method, path, parameters, function(done) {
+						var qs = [];
+						switch(method) {
+						case "GET","DELETE":
+							for(var id in parameters)
+								if(Object.prototype.hasOwnProperty.call(parameters, id) && !done[id])
+									qs.push(id + "=" + encodeURIComponent(JSON.stringify(parameters[id])));
+							break;
+						}
+						return qs;
+					}, callback);
+				};
+			});
+		}
+	};
+}
